0e8d5d4af4c2984fc7dcdad1c5ee7f2331069ee3
Igor Juan Cordeiro Da Costa <igorjuancc@gmail.com>
2026-05-04 19:15:53 -0300

    feature/cadastro_produto: ajuste de pagina home e rotas

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SharedModule } from './shared/shared.module';
 import { ManterProduto } from './produto/manter-produto/manter-produto';
+import { Home } from './home';
 
 @NgModule({
-  declarations: [App, ManterProduto],
+  declarations: [App, ManterProduto, Home],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManterProduto } from './produto/manter-produto';
+import { Home } from './home';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path: '',
+    component: Home,
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'produtos/novo',
+    component: ManterProduto
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
